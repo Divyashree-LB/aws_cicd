4f8404325664e14691c71b41fb06256ab3bb4638
Ubuntu <ubuntu@ip-172-31-14-27.ap-south-1.compute.internal>
2025-11-24 10:10:29 +0000

    Updated app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,5 +7,5 @@ def home():
     return "Hello! This Flask app is running on port 6000 and deployed on EC2 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000)
+    app.run(host="0.0.0.0", port=8000)
 
